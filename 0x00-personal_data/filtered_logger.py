e5dd69271546b22cb118aef0f11c8c607df2b243
BirtukanK <birtukankuma1113@gmail.com>
2024-05-30 12:54:40 +0000

    using logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Defines Regex-ing"""
+import logging
 import re
 from typing import List
 
@@ -9,3 +10,21 @@ def filter_datum(fields: List[str], redaction: str,
     """ returns the log message obfuscated"""
     return re.sub(f'({"|".join(fields)})=[^{separator}]*',
                   lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
