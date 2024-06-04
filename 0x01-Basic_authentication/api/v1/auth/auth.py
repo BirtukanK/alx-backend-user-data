578b2644ca2918723da4ac9991a8c82cfd4d5764
BirtukanK <birtukankuma1113@gmail.com>
2024-06-04 07:00:46 +0000

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,17 +11,16 @@ class Auth:
     """ auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ authentication required"""
-        if path is not excluded_paths:
-            return True
         if path is None:
             return True
-        if not excluded_paths or excluded_paths is None:
+        if len(excluded_paths) == 0 or excluded_paths is None:
             return True
         normalized_path = path.rstrip('/')
         normalized_excluded_paths = [excluded_path.rstrip('/')
                                      for excluded_path in excluded_paths]
         if normalized_path in normalized_excluded_paths:
             return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header"""
