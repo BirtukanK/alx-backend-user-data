df2b2d78f778f56768a02273659876aea723944f
BirtukanK <birtukankuma1113@gmail.com>
2024-06-04 06:55:03 +0000

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" auth file
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ authentication required"""
+        if path is not excluded_paths:
+            return True
+        if path is None:
+            return True
+        if not excluded_paths or excluded_paths is None:
+            return True
+        normalized_path = path.rstrip('/')
+        normalized_excluded_paths = [excluded_path.rstrip('/')
+                                     for excluded_path in excluded_paths]
+        if normalized_path in normalized_excluded_paths:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method"""
+        return None
