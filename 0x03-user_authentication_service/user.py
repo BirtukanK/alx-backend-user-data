6086939179bc443a1bd3bb921ec5442e6aec2985
BirtukanK <birtukankuma1113@gmail.com>
2024-06-11 06:38:16 +0000

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,17 +3,20 @@
 
 
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class User(Base):
     """ user class to define model"""
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key = True)
-    email = Column(String, nullable = False)
-    hashed_password = Column(String, nullable = False)
-    session_id = Column(String, nullable = True)
-    reset_token = Column(String, nullable = False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=False)
 
     def __repr__(self):
         """ display values"""
